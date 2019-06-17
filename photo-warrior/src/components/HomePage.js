0502158c387b14a660dafe0834d3eb058165d008
Heather Richardson <heatherrichardsonphoto@gmail.com>
2019-06-17 23:59:19 +0200

    changes to landing page

--- photo-warrior/src/components/HomePage.js
+++ photo-warrior/src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import GlitchEffect from 'react-glitch-effect';
+// import GlitchEffect from 'react-glitch-effect';
 // import { Switch, Route, NavLink } from "react-router-dom";
 // import LoginPage from "./LoginPage";
 
@@ -8,45 +8,21 @@ class HomePage extends Component {
   render() {
     return (
     <header className="App-header">
-    <section class="container">
-    <div class= "columns">
-      <div class="left-column">
-     
-        <img class="title" src="images/photowarriorname.png" alt="warrior"/>
-       
-        <h2 class= "subtitle">Awaken the inner warrior and slay your galleries.
-        Photo culling has never been easier.
-        </h2>
+
+      <div class="landing" >
+    
+        <img class="warrior" src="images/whitelogo.png" alt="warrior"/>
+        </div>
+
         <div class="button-group" > 
-        
         <div >
-        < GlitchEffect onHover={true}>
         <a href="/login-page"><button class="buttons">LOG IN </button></a>
-        </ GlitchEffect>
            </div> 
 
-        <div class="control">
-
-        < GlitchEffect onHover={true}>
+        <div class="control"/>
         <a href="/signup-page"><button class="buttons">SIGN UP </button></a>
-        </ GlitchEffect>
-
-      </div> 
-           </div>
-           </div>
-      {/* <hr class="spacer is-1-5"/> */}
-      <div class= "right-column">
       
-        <img class="warrior" src="images/pwlogowhite.png" alt="logo"/>  
-       
-        </div>
- 
-        {/* {/* <LoginPage currentUser={this.props.currentUser}
-                onUserChange={this.props.onUserChange} /> */}
-        {/* <p><NavLink to="/signup-page">Not a warrior yet?  SIGN UP.</NavLink></p> */}
       </div>
-      </section>  
-      
       </header>
     );
   }
